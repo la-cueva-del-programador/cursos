49040f8be183f02494e0bebe12f00d53361a64e6
la-cueva-del-programador <54548947+la-cueva-del-programador@users.noreply.github.com>
2019-08-26 12:29:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,58 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
 <head>
-  <title>La cueva del programador virgen</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LA CUEVA DEL PROGRAMADOR VIRGEN</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-<center><h1>LO MEJOR DE LO MEJOR</h1></center>
-<video>
-<source src="ALADDIN.mp4" type="video/mp4">
-</video>
+    <header>
+        <div class="contenedor">
+            <nav class="menu">
+                <a href="">Inicio</a>
+                <a href="">Acerca de</a>
+                <a href="">Portafolio</a>
+                <a href="">Contacto</a>
+            </nav>
+            <div class="contenido-header">
+                <h1 class="titulo-header">LA CUEVA DEL PROGRAMADOR VIRGEN</h1>
+                <span class="linea-header"></span>
+                <h2 class="texto-header">Lorem ipsum, dolor sit amet consectetur <span class="desaparecer">adipisicing elit. Nobis esse odit commodi, ipsa optio quos!</span></h2>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="portafolio">
+            <div class="contenedor">
+                <div class="portafolio-textos">
+                    <h2>Haz de tu trabajo algo extraordinario</h2>
+                    <p>Contactame si estás preparado</p>
+                </div>
+                <section class="galeria">
+                    <div class="fotos">
+                        <img src="img/1.jpg" alt="">
+                    </div>
+                     <div class="fotos">
+                        <img src="img/2.jpg" alt="">
+                    </div>
+                    <div class="fotos">
+                        <img src="img/3.jpg" alt="">
+                    </div>
+                    <div class="fotos">
+                        <img src="img/4.jpg" alt="">
+                    </div>
+                    <div class="fotos">
+                        <img src="img/5.jpg" alt="">
+                    </div>
+                    <div class="fotos">
+                        <img src="img/6.jpg" alt="">
+                  </div>
+                </section>
+            </div>
+        </section>
+    </main>
+    
 </body>
 </html>
